376933474f87a153939e1d1932fa4947b00d16e7
fanghao <fanghao@ctirobot.com>
2021-08-18 12:01:26 +0800

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,11 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <h1>{{title}}</h1>
   </div>
 </template>
 
-<script lang="ts">
-import { Options, Vue } from 'vue-class-component'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+<script lang="ts" setup>
+import { ref } from 'vue'
 
-@Options({
-  components: {
-    HelloWorld
-  }
-})
-export default class Home extends Vue {}
+const title = ref('TODOS')
 </script>
